52f7635c9db8dce120afc047ffd139dfed2223d7
Ly Dollar <146605742+DollieNOMONEY@users.noreply.github.com>
2025-07-15 22:54:15 +0700

    fixed eslint 10

--- src/app/cars/[carmake]/[id]/page.tsx
+++ src/app/cars/[carmake]/[id]/page.tsx
@@ -2,11 +2,6 @@ import { getCarById } from "@/app/_actions/carActions";
 import CarDetailClient from "@/components/CarDetailClient";
 import { notFound } from "next/navigation";
 
-interface Params {
-  carmake: string;
-  id: string;
-}
-
 export default async function CarPage({
   params,
 }: {
